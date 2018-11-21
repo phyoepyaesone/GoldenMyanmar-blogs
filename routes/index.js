117bebb724a34ef629396e96b67825196112f94d
phyoepyaesone <naylinnoo322@gmail.com>
2018-11-21 09:06:41 +0630

    add view signin signup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('commons/signup.ejs', { title: 'SignUp' });
+});
+
+
+router.get('/signin', function(req, res, next) {
+  res.render('commons/signin.ejs', { title: 'SignIn' });
+});
+
 module.exports = router;
